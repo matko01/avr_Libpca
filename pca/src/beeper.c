4d06896b358ff3000dec2cb89fc15548f9bcf109
Tomasz Wisniewski <tomasz.wisni3wski@gmail.com>
2013-08-14 22:24:24 +0000

    cleanup3

--- pca/src/beeper.c
+++ pca/src/beeper.c
@@ -23,6 +23,30 @@ static void _find_ocr_prescaler(uint32_t *ocr, uint8_t *prescaler, uint32_t a_fr
 }
 
 /**
+ * @brief calculate prescaler and OCR value for 8 bit counters
+ *
+ * @param a_freq requested frequency
+ * @param a_ocr ocr value returned by reference
+ * @param a_prescaler prescaler value returned by reference
+ */
+static void _timer8_prescaler(uint32_t a_freq, 
+		uint8_t *a_ocr, 
+		uint8_t *a_prescaler) {
+
+	uint32_t ocr = 0x00;
+
+	// smallest achievable frequency with this timer
+	if (a_freq < 62) {
+		*a_prescaler = 5;
+		*a_ocr = 255;
+		return;
+	}
+
+	_find_ocr_prescaler(&ocr, a_prescaler, a_freq, 255);
+	*a_ocr = (ocr & 0xff);	
+}
+
+/**
  * @brief calculate prescaler and ocr values for 16 bit counter
  *
  * @param a_freq requested frequency
@@ -30,7 +54,7 @@ static void _find_ocr_prescaler(uint32_t *ocr, uint8_t *prescaler, uint32_t a_fr
  * @param a_ocrl ocr low
  * @param a_prescaler prescaler value
  */
-static void _timer_prescaler(uint32_t a_freq,
+static void _timer16_prescaler(uint32_t a_freq,
 		uint8_t *a_ocrh,
 		uint8_t *a_ocrl,
 		uint8_t *a_prescaler) {
@@ -53,13 +77,8 @@ static void _timer_prescaler(uint32_t a_freq,
 		return;
 	}
 
-	if (!a_ocrh) {
 		_find_ocr_prescaler(&ocr, a_prescaler, a_freq, 512);
-	}
-	else {
-		_find_ocr_prescaler(&ocr, a_prescaler, a_freq, 255);
-		*a_ocrh = (ocr >> 8) & 0xff;
-	}
+	*a_ocrh = (ocr & 0xff00) >> 8;
 
 	*a_ocrl = (ocr & 0xff);
 }
@@ -81,7 +100,8 @@ void beeper_init(e_timer a_timer) {
 #if TDELAY_IMPLEMENT_T1_INT == 1
 		case E_TIMER1:
 			DDRB |= _BV(PORTB1);
-			TCCR1A = 0x40;
+			TCCR1A |= 0x40;
+			TCCR1B |= 0x08;
 			break;
 #endif
 
@@ -104,25 +124,21 @@ void beeper_beep(e_timer a_timer,
 	uint32_t freq, 
 	uint32_t duration) {
 	
+	uint8_t ocr = 0x00;
+	uint8_t ocrh = 0x00;
+	uint8_t presc = 0x00;
 
 	beeper_off(a_timer);
 	_tdc_setup_ms(a_timer, duration);
 
-	if (!freq) {
-		// settings correction
-		_tdc_set_duration(a_timer, (uint32_t)((freq*duration) / 500));
-	}
-	else {
-		uint8_t ocr = 0x00;
-		uint8_t ocrh = 0x00;
-		uint8_t presc = 0x00;
+	if (freq) {
 
 		switch(a_timer) {
 
 #if TDELAY_IMPLEMENT_T0_INT == 1
 			case E_TIMER0:
 				TCCR0A |= 0x40;
-				_timer_prescaler(freq, NULL, &ocr, &presc);
+				_timer8_prescaler(freq, &ocr, &presc);
 				TCCR0B = 0x00 | (presc & 0x07);
 				OCR0A = ocr;
 				freq = freq < 62 ? 62 : freq;
@@ -131,21 +147,18 @@ void beeper_beep(e_timer a_timer,
 
 #if TDELAY_IMPLEMENT_T1_INT == 1
 			case E_TIMER1:
-				_timer_prescaler(freq, 
-						&ocrh, 
-						&ocr, 
-						&presc);
+				_timer16_prescaler(freq, &ocrh, &ocr, &presc);
 
 				OCR1AH = ocrh;
 				OCR1AL = ocr;
 				TCCR1A |= 0x40;
-				TCCR1B = 0x08 | presc;
+				TCCR1B = 0x08 | (presc & 0x07);
 				break;
 #endif
 
 #if TDELAY_IMPLEMENT_T2_INT == 1
 			case E_TIMER2:
-				_timer_prescaler(freq, NULL, &ocr, &presc);
+				_timer8_prescaler(freq, &ocr, &presc);
 				OCR2A = ocr;
 				TCCR2A |= 0x40;
 				TCCR2B = 0x00 | (presc & 0x07);
@@ -157,6 +170,8 @@ void beeper_beep(e_timer a_timer,
 				return;
 				break;
 		} // switch
+		// settings correction
+		_tdc_set_duration(a_timer, (uint32_t)((freq*duration) / 500));
 	}	
 
 	_tdc_enable_interrupt(a_timer);
@@ -190,4 +205,8 @@ void beeper_off(e_timer a_timer) {
 			break;
 	} // switch
 	_tdc_set_duration(a_timer, 0x00);
+}
+
+void beeper_block(e_timer a_timer) {
+	_tdc_block(a_timer);
 }
