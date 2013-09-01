dde67d30459daa53fb5b285135f260174fbf2da2
Tomasz Wisniewski <tomasz.wisni3wski@gmail.com>
2013-09-01 17:50:43 +0000

    pca common header

---
+++ pca/include/pca.h
@@ -0,0 +1,18 @@
+#ifndef PCA_H_YDRXPN0M
+#define PCA_H_YDRXPN0M
+
+#include "config.h"
+#include "common.h"
+
+#include "serial.h"
+#include "slip.h"
+#include "timer_common.h"
+#include "tdelay_common.h"
+
+#include "tdelay.h"
+#include "beeper.h"
+#include "tpwm.h"
+
+
+#endif /* PCA_H_YDRXPN0M */
+
