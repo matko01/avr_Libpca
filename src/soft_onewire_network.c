c313918e6856925a55b7172d38367ecb89754213
Tomasz Wisniewski <tomasz.wisni3wski@gmail.com>
2013-11-14 16:08:42 +0000

    wip

--- src/soft_onewire_network.c
+++ src/soft_onewire_network.c
@@ -8,6 +8,9 @@ uint8_t ow_net_search_rom(soft_ow *a_bus, uint8_t max_devices, ow_romcode_t *a_r
 
 	uint8_t bit_index = 0x00;
 	uint8_t dev_cnt = 0x00;
+	
+	/* B | A */
+	uint8_t bits = 0x00;
 
 #ifdef DESIGN_BY_CONTRACT	
 	// input parameters validation
@@ -26,6 +29,21 @@ uint8_t ow_net_search_rom(soft_ow *a_bus, uint8_t max_devices, ow_romcode_t *a_r
 		return 0;
 	}
 
+	memset(a_romcodes, 0x00, max_devices * sizeof(ow_romcode_t));
 	sow_write_byte(a_bus, OWN_SEARCH_ROM);
+
+	bits = _sow_read_bit(a_bus); // bit A
+	bits |= (_sow_read_bit(a_bus) << 4); // bit B
+
+	if (0x11 == bits) {
+		// two ones indicate no response from the devices (no one pulled the line low)
+		return 0;
+	}
+
+	// we've two zeros - there's a address bit conflict
+	if (!bits) {
+	}
+
+
 
 }
