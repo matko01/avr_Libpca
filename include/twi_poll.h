da1c4a8647dbe92320d9f9fd5a14df79fbb1d17f
TW <tomasz@wisni3wski@gmail.com>
2013-11-29 15:45:16 +0000

    twi_poll skeleton created

---
+++ include/twi_poll.h
@@ -0,0 +1,14 @@
+#ifndef __TWI_POLL_H__
+#define __TWI_POLL_H__
+
+#include "config.h"
+#include "common.h"
+#include "twi_common.h"
+
+void twi_poll_init(uint8_t a_freq);
+// twi_poll_mtx()
+// twi_poll_mrx()
+// twi_poll_search_devices()
+
+
+#endif /* __TWI_POLL_H__ */
---
+++ src/twi_poll.c
@@ -0,0 +1,5 @@
+#include "twi_poll.h"
+
+void twi_poll_init(uint8_t a_freq) {
+}
+
